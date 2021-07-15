a8aadfcd1773d261968a65003198694a88a8c02a
Ysj <543501451@qq.com>
2021-07-15 11:54:18 +0800

    <新增> 用于标识是 YCR 自动生成的内容的注解

---
+++ libs/lib_ycr_annotation/src/main/java/com/ysj/lib/ycr/annotation/YCRGenerated.kt
@@ -0,0 +1,11 @@
+package com.ysj.lib.ycr.annotation
+
+/**
+ * 标识是 YCR 自动生成的内容。用于 [YCR-Idea-Plugin](https://github.com/Ysj001/YCR-Idea-Plugin)
+ *
+ * @author Ysj
+ * Create time: 2020/8/4
+ */
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.BINARY)
+annotation class YCRGenerated
