eccc187766668c53632e9c5267b00175c30e24d9
Ysj <543501451@qq.com>
2021-07-15 11:52:45 +0800

    <完善> 配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,3 @@
-val localProps = java.util.Properties().apply {
-    val file = File("local.properties")
-    if (!file.exists()) return@apply
-    file.bufferedReader().use { load(it) }
-}
-
 rootProject.name = "YCR"
 
 // libs
@@ -13,13 +7,10 @@ include(":lib_ycr_apt")
 project(":lib_ycr_apt").projectDir = File(rootDir, "libs/lib_ycr_apt/")
 include(":lib_ycr_plugin")
 project(":lib_ycr_plugin").projectDir = File(rootDir, "libs/lib_ycr_plugin/")
-if (localProps["useRelease"] == "true") {
-    include(":lib_ycr_api")
-    project(":lib_ycr_api").projectDir = File(rootDir, "libs/lib_ycr_api/")
-} else {
-    include(":lib_ycr_api_dev")
-    project(":lib_ycr_api_dev").projectDir = File(rootDir, "libs/lib_ycr_api_dev/")
-}
+//include(":lib_ycr_api")
+//project(":lib_ycr_api").projectDir = File(rootDir, "libs/lib_ycr_api/")
+include(":lib_ycr_api_dev")
+project(":lib_ycr_api_dev").projectDir = File(rootDir, "libs/lib_ycr_api_dev/")
 
 // demo
 include(":app")
