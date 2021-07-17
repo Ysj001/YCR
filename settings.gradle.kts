9f51f193c74f024673781a54596fe02299f52b69
Ysj <543501451@qq.com>
2021-07-17 23:22:55 +0800

    <修改> 默认不 include 源码工程到项目中

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
 rootProject.name = "YCR"
 
 // libs
-include(":lib_ycr_annotation")
-project(":lib_ycr_annotation").projectDir = File(rootDir, "libs/lib_ycr_annotation/")
-include(":lib_ycr_apt")
-project(":lib_ycr_apt").projectDir = File(rootDir, "libs/lib_ycr_apt/")
-include(":lib_ycr_plugin")
-project(":lib_ycr_plugin").projectDir = File(rootDir, "libs/lib_ycr_plugin/")
+//include(":lib_ycr_annotation")
+//project(":lib_ycr_annotation").projectDir = File(rootDir, "libs/lib_ycr_annotation/")
+//include(":lib_ycr_apt")
+//project(":lib_ycr_apt").projectDir = File(rootDir, "libs/lib_ycr_apt/")
+//include(":lib_ycr_plugin")
+//project(":lib_ycr_plugin").projectDir = File(rootDir, "libs/lib_ycr_plugin/")
 //include(":lib_ycr_api")
 //project(":lib_ycr_api").projectDir = File(rootDir, "libs/lib_ycr_api/")
-include(":lib_ycr_api_dev")
-project(":lib_ycr_api_dev").projectDir = File(rootDir, "libs/lib_ycr_api_dev/")
+//include(":lib_ycr_api_dev")
+//project(":lib_ycr_api_dev").projectDir = File(rootDir, "libs/lib_ycr_api_dev/")
 
 // demo
 include(":app")
