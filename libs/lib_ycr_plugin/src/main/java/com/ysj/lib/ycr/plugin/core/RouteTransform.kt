4ebdc429b0632c2a22b65b4926141a0c4327bd8a
Ysj <543501451@qq.com>
2021-07-24 15:26:00 +0800

    <完善> Transform 过滤逻辑

--- libs/lib_ycr_plugin/src/main/java/com/ysj/lib/ycr/plugin/core/RouteTransform.kt
+++ libs/lib_ycr_plugin/src/main/java/com/ysj/lib/ycr/plugin/core/RouteTransform.kt
@@ -15,10 +15,12 @@ import org.objectweb.asm.ClassWriter
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
+import java.util.*
 import java.util.jar.JarEntry
 import java.util.jar.JarFile
 import java.util.jar.JarOutputStream
 import java.util.zip.ZipEntry
+import kotlin.collections.HashSet
 
 /**
  * 路由的 Transform
@@ -111,32 +113,30 @@ class RouteTransform(private val project: Project) : Transform() {
             input.scopes,
             Format.JAR
         )
-        if (notNeedJarEntriesCache.contains(src.name)) {
+        if (src.name in notNeedJarEntriesCache) {
             FileUtils.copyFile(src, dest)
             return
         }
         JarFile(src).use { jf ->
             JarOutputStream(dest.outputStream()).use { jos ->
-                jf.entries().toList()
-                    .filter { true }
-                    .forEach {
-                        jf.getInputStream(it).use { ips ->
-                            val zipEntry = ZipEntry(it.name)
-                            if (it.name.endsWith(".class")) {
-                                //                    logger.quiet("process jar element --> ${element.name}")
-                                val cr = ClassReader(ips)
-                                val cw = ClassWriter(cr, ClassWriter.COMPUTE_MAXS)
-                                val cv = AllClassVisitor(this, cw)
-                                cr.accept(cv, ClassReader.EXPAND_FRAMES)
-                                jos.putNextEntry(zipEntry)
-                                jos.write(cw.toByteArray())
-                            } else {
-                                jos.putNextEntry(zipEntry)
-                                jos.write(ips.readBytes())
-                            }
-                            jos.closeEntry()
+                jf.entries().forEach {
+                    jf.getInputStream(this).use { ips ->
+                        val zipEntry = ZipEntry(name)
+                        if (notNeedJarEntries()) {
+                            jos.putNextEntry(zipEntry)
+                            jos.write(ips.readBytes())
+                        } else {
+//                            logger.quiet("process jar element --> $name")
+                            val cr = ClassReader(ips)
+                            val cw = ClassWriter(cr, 0)
+                            val cv = AllClassVisitor(this@RouteTransform, cw)
+                            cr.accept(cv, 0)
+                            jos.putNextEntry(zipEntry)
+                            jos.write(cw.toByteArray())
                         }
+                        jos.closeEntry()
                     }
+                }
             }
         }
     }
@@ -150,78 +150,75 @@ class RouteTransform(private val project: Project) : Transform() {
             Format.DIRECTORY
         )
         FileUtils.copyDirectory(src, dest)
-        dest.walk()
-            .filter { isNeedFile(it) }
-            .forEach {
-//                logger.quiet("process file --> ${it.name}")
-                it.inputStream().use { fis ->
-                    val cr = ClassReader(fis)
-                    val cw = ClassWriter(cr, 0)
-                    val cv = AllClassVisitor(this, cw)
-                    cr.accept(cv, ClassReader.EXPAND_FRAMES)
-                    FileOutputStream(it).use { fos ->
-                        fos.write(cw.toByteArray())
-                    }
+        dest.walk().forEach {
+            if (!it.isNeedFile()) return@forEach
+//            logger.quiet("process file --> ${it.name}")
+            it.inputStream().use { fis ->
+                val cr = ClassReader(fis)
+                val cw = ClassWriter(cr, 0)
+                val cv = AllClassVisitor(this, cw)
+                cr.accept(cv, ClassReader.EXPAND_FRAMES)
+                FileOutputStream(it).use { fos ->
+                    fos.write(cw.toByteArray())
                 }
             }
+        }
     }
 
     private fun prePrecess(jis: Collection<JarInput>, dis: Collection<DirectoryInput>) {
         jis.forEach { input ->
             JarFile(input.file).use { jf ->
-                val entries = jf.entries().toList()
                 /*
                     由于该 transform 可能后于其他 transform
                     此时该 transform 的输入源会变为其他 transform 的输出
                     此时输入源的名称会发生变化，因此不能简单通过 input.file.name 过滤
                  */
-                if (notNeedJarEntries(entries)) {
-                    notNeedJarEntriesCache.add(input.file.name)
-                    return@forEach
-                }
-                entries.toList()
-                    .filter { it.name.endsWith(".class") }
-                    .forEach {
-                        logger.verbose("need process in jar --> ${it.name}")
-                        preVisitor(jf.getInputStream(it))
+                val entries = jf.entries()
+                entries.forEach entry@{
+                    if (notNeedJarEntries()) {
+                        if (!entries.hasMoreElements()) notNeedJarEntriesCache.add(input.file.name)
+                        return@entry
                     }
+                    logger.verbose("need process in jar --> ${name}")
+                    jf.getInputStream(this).preVisitor()
+                }
             }
         }
         dis.forEach { input ->
-            input.file.walk()
-                .filter { isNeedFile(it) }
-                .forEach {
-                    logger.verbose("need process in dir --> ${it.name}")
-                    preVisitor(it.inputStream())
-                }
+            input.file.walk().forEach test@{
+                if (!it.isNeedFile()) return@test
+                logger.verbose("need process in dir --> ${it.name}")
+                it.inputStream().preVisitor()
+            }
         }
     }
 
-    private fun preVisitor(inputStream: InputStream) {
-        inputStream.use {
-            val cr = ClassReader(it)
-            val cw = ClassWriter(cr, 0)
-            val cv = PreVisitor(this, cw)
-            cr.accept(cv, ClassReader.EXPAND_FRAMES)
-        }
+    private fun InputStream.preVisitor() = use {
+        val cr = ClassReader(it)
+        val cw = ClassWriter(cr, 0)
+        val cv = PreVisitor(this@RouteTransform, cw)
+        cr.accept(cv, 0)
     }
 
-    private fun notNeedJarEntries(entries: List<JarEntry>): Boolean {
-        entries.forEach {
-            if (it.name.startsWith("kotlin/")
-                || it.name.startsWith("kotlinx/")
-                || it.name.startsWith("org/intellij/")
-                || it.name.startsWith("org/jetbrains/")
-                || it.name.startsWith("androidx/")
-                || it.name.startsWith("android/")
-                || it.name.startsWith("com/google/android/")
-            ) return true
-        }
-        return false
-    }
+    private fun JarEntry.notNeedJarEntries(): Boolean =
+        name.endsWith(".class").not()
+                || name.startsWith("kotlin/")
+                || name.startsWith("kotlinx/")
+                || name.startsWith("javax/")
+                || name.startsWith("org/intellij/")
+                || name.startsWith("org/jetbrains/")
+                || name.startsWith("org/junit/")
+                || name.startsWith("org/hamcrest/")
+                || name.startsWith("com/squareup/")
+                || name.startsWith("androidx/")
+//                || name.startsWith("android/")
+                || name.startsWith("com/google/android/")
 
-    private fun isNeedFile(file: File): Boolean =
-        file.isFile && file.extension == "class" && file.name !in listOf("BuildConfig.class")
+    private fun File.isNeedFile(): Boolean = isFile && extension == "class"
+
+    private inline fun <T> Enumeration<T>.forEach(block: T.() -> Unit) {
+        while (hasMoreElements()) nextElement().block()
+    }
 
     private inline fun doTransform(
         transformInvocation: TransformInvocation,
